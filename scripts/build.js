2deac65d71702a4ba8be26fca3bb1f848766146e
Devin <devthedevel@gmail.com>
2024-04-24 17:29:48 -0230

    Fixed building issues

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,21 @@
-import { writeFile } from 'fs/promises'
+import { writeFile, mkdir } from 'fs/promises'
+import { join } from 'path';
+import { cwd } from 'process';
 
-await writeFile('./lib/cjs/package.json', JSON.stringify({
-    type: 'commonjs'
-}))
+async function main() {
+    const LIB_PATH = join(cwd(), 'lib');
+    const CJS_PATH = join(LIB_PATH, 'cjs');
+    const ESM_PATH = join(LIB_PATH, 'esm');
 
-await writeFile('./lib/esm/package.json', JSON.stringify({
-    type: 'module'
-}))
+    await mkdir(CJS_PATH, { recursive: true })    
+    await writeFile(join(CJS_PATH, 'package.json'), JSON.stringify({
+        type: 'commonjs'
+    }))
+    
+    await mkdir(ESM_PATH, { recursive: true })
+    await writeFile(join(ESM_PATH, 'package.json'), JSON.stringify({
+        type: 'module'
+    }))
+}
+
+main().then(() => console.log('Done building'))
